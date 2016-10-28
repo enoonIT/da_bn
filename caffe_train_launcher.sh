a9b1b502f30041421e2432e433f7db454773e3fb
Fabio Maria Carlucci <fabiom.carlucci@yahoo.it>
2016-10-28 17:11:13 +0200

    train launcher

---
+++ caffe_train_launcher.sh
@@ -0,0 +1,11 @@
+CAFFE_HOME=/home/enoon/libs/bvlc_caffe/
+OUT_DIR=output_dir
+for line in $(find ${1} -maxdepth 2 -mindepth 2 -type d -not -path '*/\.*'); do 
+     cd $line
+     echo Starting $line
+     ${CAFFE_HOME}/build/tools/caffe train -solver solver.prototxt -weights ${CAFFE_HOME}/models/bvlc_alexnet/bvlc_alexnet.caffemodel 2>&1 | tee caffe_output.txt
+     cd -
+done
+mkdir $OUT_DIR -p
+python $CAFFE_HOME/tools/extra/parse_log.py caffe_output.txt ${OUT_DIR}
+#python ~/bin/caffe_log_viewer.py ${OUT_DIR}/${NAME}.train ${OUT_DIR}/${NAME}.test ${OUT_DIR}/out_${2}.png
